0232f59ea9e80de167646e0efd91773f464d2ba0
buuhq1981 <buuhq1981@gmail.com>
2024-10-06 21:03:11 +0700

    1st

---
+++ 000-template/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n; cin >> n;
+    // int a[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    // }
+    
+    
+    cout << "hello cpp !!! " << endl;
+    cout << n << endl;
+    return 0;
+}
